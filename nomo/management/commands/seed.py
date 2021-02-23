6c7ac4344e64dcb1719721fbce242ef81f9cd671
michellecgude <michellecgude@gmail.com>
2021-02-23 12:35:54 -0600

    built out user model.

---
+++ nomo/management/commands/seed.py
@@ -0,0 +1,6 @@
+# the library to retrieve information from a website
+# must run "pip install requests" to download.
+
+import requests
+from django.core.management.base import Basecommand
+# from ...models import Nomo_Model
--- nomo/models.py
+++ nomo/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+# User Model
+class User(models.Model):
+    name = models.CharField(max_length=100, blank=True)
+    dob = models.DateField((""), auto_now=False, auto_now_add=False)
+    bio = models.CharField((""), max_length=500)
+
+
+# Timecapsule Model
+
+
+
+
+
+# API Model
+
+
+
+
+
+
+
+
+def __str__(self):
+    return self.name
