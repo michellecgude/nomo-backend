99dc238956c03ed1f6a3c48e23252f8ec117b8a3
michellecgude <michellecgude@gmail.com>
2021-02-23 13:52:22 -0600

    created admin, registered models, now to build out data.

--- nomo/admin.py
+++ nomo/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import User, Events, Births, Deaths
+
+admin.site.register((User, Events, Births, Deaths))
--- nomo_django/urls.py
+++ nomo_django/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # path('api-auth', include('rest_framework.urls', namespace='rest_framework'))
 ]
